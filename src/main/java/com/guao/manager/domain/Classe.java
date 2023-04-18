b915bd0239358a8e36abbb3328787b5a224b3a4b
= <=>
2023-04-18 20:48:57 +0100

    fix database adding error

--- src/main/java/com/guao/manager/domain/Classe.java
+++ src/main/java/com/guao/manager/domain/Classe.java
@@ -15,7 +15,7 @@ public class Classe implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     //@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
     //@SequenceGenerator(name = "sequenceGenerator")
     @Column(name = "id")
