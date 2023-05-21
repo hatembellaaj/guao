f4cfa4510d72522820d1b867d0b85fbca1ea9233
ThameurBenAbdallah <bathameur@gmail.com>
2023-05-21 23:33:42 +0100

    Fance lifecycle hooks, don't forget to change db password

--- src/main/webapp/app/home/bar-chart/bar-chart.component.ts
+++ src/main/webapp/app/home/bar-chart/bar-chart.component.ts
@@ -1,41 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 import { AbsenceService } from 'app/entities/absence/service/absence.service';
 import Chart from 'chart.js/auto';
+import { forkJoin, Subscription } from 'rxjs';
 @Component({
   selector: 'jhi-bar-chart',
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.scss'],
 })
-export class BarChartComponent implements OnInit {
+export class BarChartComponent implements OnInit, OnDestroy, AfterViewInit {
   public chart: any;
   public countAbscence: number = 0;
   public countAbscenceJustifiees: number = 0;
   public abscenceData: number[] = [0, 0];
-
-  ngOnInit(): void {
-    this.absenceService.getCountAbscence().forEach(a => {
-      console.log('all=', a);
-      this.countAbscence = a;
-    });
-    this.absenceService.getCountAbscenceJustifie().forEach(a => {
-      console.log('justifie=', a);
-      this.countAbscenceJustifiees = a;
-    });
-    this.abscenceData[0] = this.countAbscenceJustifiees;
-    this.abscenceData[1] = this.countAbscence - this.countAbscenceJustifiees;
-
-    this.createChart();
-    console.log('absecence array : ', this.abscenceData);
-  }
+  private chartSubscription: Subscription | null = null;
 
   constructor(protected absenceService: AbsenceService) {}
 
-  createChart(): void {
-    this.chart = new Chart('MyChart', {
-      type: 'pie', //this denotes tha type of chart
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      if (!this.chart) {
+        this.createChart();
+      }
+    });
+    console.log(this.abscenceData);
+  }
 
+  ngOnInit(): void {
+    this.fetchAbsenceCounts();
+    this.createChart();
+  }
+  ngOnDestroy(): void {
+    if (this.chartSubscription) {
+      this.chartSubscription.unsubscribe();
+      if (this.chart) {
+        this.chart.destroy();
+      }
+    }
+  }
+
+  fetchAbsenceCounts(): void {
+    const countAbscence$ = this.absenceService.getCountAbscence();
+    const countAbscenceJustifiees$ = this.absenceService.getCountAbscenceJustifie();
+    this.chartSubscription = forkJoin([countAbscence$, countAbscenceJustifiees$]).subscribe(([countAbscence, countAbscenceJustifiees]) => {
+      this.countAbscence = countAbscence;
+      this.countAbscenceJustifiees = countAbscenceJustifiees;
+      this.updateChart();
+    });
+  }
+
+  updateChart(): void {
+    this.abscenceData[0] = this.countAbscenceJustifiees;
+    this.abscenceData[1] = this.countAbscence - this.countAbscenceJustifiees;
+
+    if (this.chart) {
+      this.chart.data.datasets[0].data = this.abscenceData;
+      this.chart.update();
+    }
+  }
+
+  createChart(): void {
+    const canvas = document.getElementById('MyChart') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+
+    if (!ctx) {
+      console.error('Failed to get 2D rendering context');
+      return;
+    }
+
+    this.chart = new Chart(ctx, {
+      type: 'pie',
       data: {
-        // values on X-Axis
         labels: ['Abscence justifiées', 'Abscence non justifiées'],
         datasets: [
           {
